0248433a32cb6fe6d96cb8e7ecd7015698ac41d2
k2511 <bhongale.k2511@gmail.com>
2025-09-09 13:05:16 +0530

    Chnages

--- Estore-backend/src/routes/salesRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getSalesSummary } from "../controllers/salesController.js";
-
-const router = express.Router();
-
-// GET /api/sales/summary
-router.get("/summary", getSalesSummary);
-
-export { router as salesRouter };
